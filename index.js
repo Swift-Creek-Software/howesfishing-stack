49dad8dc43761a9f85e8d881180b5842a0eeda20
Andrew Gross <grossmail1@gmail.com>
2017-10-10 22:08:07 -0600

    basic texting framework

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser');
+
 const app = express()
+
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
 	res.send({hi: 'der'})
 })
--- routes/textRoutes.js
+++ routes/textRoutes.js
@@ -3,13 +3,13 @@ const keys = require('../config/keys')
 
 const client = new twilio(keys.twilioAccountSid, keys.twilioAuthToken);
 module.exports = (app) => {
-app.get('/text', (req, res) => {
-	console.log('hit the text route')
+app.post('/text', (req, res) => {
+	console.log('hit the text route', )
 
-
+const body = req.body
 	client.messages.create({
-		body: 'Hello from Node',
-		to: '+14062708435',  // Text this number
+		body: body.body,
+		to: '+' + body.number,  // Text this number +
 		from:  keys.twilioFrom// From a valid Twilio number
 	})
 		.then((message) => {
