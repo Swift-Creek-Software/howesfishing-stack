c736a8535c12abc289018ef3d7aa1bedcf1d2069
Andrew Gross <grossmail1@gmail.com>
2018-03-27 20:41:37 -0600

    add production static assest to server

--- index.js
+++ index.js
@@ -25,5 +25,16 @@ require('./routes/textRoutes')(app)
 require('./routes/tripRoutes')(app)
 require('./routes/userRoutes')(app)
 
+if (process.env.NODE_ENV === 'production') {
+  // make sure express serves up prod assets
+  app.use(express.static('client/build'))
+
+  // serve up the index.html
+  const path = require('path')
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
