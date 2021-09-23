6d14ab66ada564737bf1bc8d37402b3d8be4b131
Andrew Gross <grossmail1@gmail.com>
2021-09-23 16:02:27 -0600

    updating reminder to send email

--- bin/reminder.js
+++ bin/reminder.js
@@ -8,43 +8,44 @@ require('../models/Trip')
 
 const Trip = mongoose.model('Trip')
 
-mongoose.connect(keys.mongoURI, { useMongoClient: true })
+mongoose.connect(keys.mongoURI)
 mongoose.Promise = global.Promise;
 
 const day = addDays(utcToZonedTime(new Date(), 'America/Denver'), 3);
 const startDay = zonedTimeToUtc(startOfDay(day), 'America/Denver')
 const endDay = zonedTimeToUtc(endOfDay(day), 'America/Denver')
+
 Trip.find({ deleted: false, sendClientEmail: true, startTime: {"$gte": startDay, "$lt": endDay} })
   .sort({ endTime: 'asc'})
   .exec((err, trips) => {
     if (err) {
       return err
     }
-
-    const params = {
-      sandbox: false,
-      recipients: [
-        {
-          address: 'grossmail1@gmail.com'
+    trips.forEach(trip => {
+      const params = {
+        recipients: [
+          {
+            address: trip.email,
+          },
+          {
+            address: {
+              'email': 'reservations@howesfishing.com',
+              'header_to': trip.email,
+            },
+          },
+        ],
+        templateId: 'howes-fishing',
+        templateData: {
+          firstName: trip.firstName,
+          confirm: 'remind',
+          directions: trip.directions,
+          from: trip.userName,
+          subject: `Fishing Trip Reminder ${format(new Date(trip.startTime), 'MM/dd/yyyy')}`,
+          timeCost: trip.clientEmailTemplate,
         },
-        {
-          address: {
-            "email": "reservations@howesfishing.com",
-            "header_to": 'grossmail1@gmail.com'
-          }
-        }
-      ],
-      templateId: 'client-follow-up',
-      templateData: {
-        trips,
-        day,
-        startDay,
-        endDay,
-        subject: `Fishing Trip Reminder ${format(day, 'MM/dd/yyyy')}`,
-      },
-      campaignId: 'Client confirmation',
-    }
-    //
-    return EmailService
-      .sendEmail(params)
+        campaignId: 'Client reminder',
+      }
+      EmailService.sendEmail(params)
+      // console.log('params',JSON.stringify(params))
+    })
   })
